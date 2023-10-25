e7389afbd33f7952ec45a71fd13b348715645a2d
AlexVasilev25 <148490379+AlexVasilev25@users.noreply.github.com>
2023-10-25 20:03:50 +0300

    add binary search

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,5 +1,6 @@
-"""Компьютер сам загадывает и сам угадывает число"""
+
 import numpy as np
+
 
 
 def random_predict(number: int=1) -> int:
@@ -24,6 +25,63 @@ def random_predict(number: int=1) -> int:
 
 
 
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+       
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+    
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+
+
+def game_core_v3(number: int = 1) -> int:
+    """Устанавливаем верхнюю и нижнюю границы поиска, алгоритм угадывает число по принципу бинарного дерева,
+    если число больше центрального числа в диапазоне поиска, тогда переопределяем нижнюю границу,
+    она становится равной загаданному числу, если число меньше, то переопределяем вернюю границу,
+    и так каждый раз сокращаем диапазон поиска в два раза.
+    Функция принимает загаданной число, и возвращает количество попыток.
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0
+    max_num = 101 # Верхняя граница поиска
+    min_num = 1 # Нижняя граница поиска
+    
+    while True:
+        count +=1  
+        mid = (max_num+min_num) // 2 # Находим центральное число в диапазоне
+        if number == mid:
+            break
+        elif mid < number:
+            min_num = mid
+        else:
+            max_num = mid        
+    # Ваш код заканчивается здесь
+    
+    return count   
+      
+      
 
 def score_game(random_predict) -> int:
     """ В среднем, за какое количество попыток, за 1000 подходов, угадывает число наш алгоритм
@@ -36,7 +94,7 @@ def score_game(random_predict) -> int:
     """
     
     count_ls = [] # Список для сохранения попыток
-    np.random.seed(1)
+    np.random.seed(1) # Фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # Массив из 1000 элементов, каждый из которых является случайным числом от 1 до 100
     
     for number in random_array:
@@ -50,7 +108,7 @@ def score_game(random_predict) -> int:
 
 # RUN
 if __name__ == '_main_': # Если главный скрипт выполняется в этом файле, условие выполняется, если код будет импортирован, фенкция запускаться не будет
-    score_game(random_predict)
+    score_game(game_core_v3)
 
 
     
